5059f71958393e56b2a084d5806ea42af0d118e3
Razhely <534355015@qq.com>
2025-11-19 10:55:53 +0800

    add base and gameworld

---
+++ GameProject/include/core/gameworld.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+#include "map.hpp"
+#include "unit.hpp"
+#include "base.hpp"
+
+class GameWorld {
+private:
+    Map map;
+    Base baseA;
+    Base baseB;
+
+    std::vector<Unit*> unitsA;
+    std::vector<Unit*> unitsB;
+
+public:
+    GameWorld(int w, int h, const Coord& baseAPos, const Coord& baseBPos);
+
+    void update(float dt);
+    void render() const;
+    void addUnit() const;
+
+    bool isFree(const Coord& c) const;
+
+
+    const std::vector<Unit*>& getEnemies(Faction f) const;
+
+    const Map& getMap() const {
+        return map;
+    }
+
+
+};
